6940fabd294e35c9c5638a23f0c3a542f2382648
Jon Loureiro <me@jonloureiro.dev>
2022-03-21 23:33:04 -0400

    chore: finish form implementation and add handleSubmit

--- front/src/pages/Information.jsx
+++ front/src/pages/Information.jsx
@@ -1,4 +1,8 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { IMaskInput } from 'react-imask';
+
+import { isValidCPF } from '../utils';
 import { config } from './config';
 import { Layout } from './__Layout';
 
@@ -9,32 +13,129 @@ const subtitle =
   'Se você não ficar feliz, devolvemos sua entrada em até 7 dias.';
 
 export function Information() {
+  const [errors, setErros] = useState({
+    name: false,
+    email: false,
+    cpf: false,
+    entry: false,
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!e.currentTarget) {
+      console.error('Erro no formulário');
+      console.error(e);
+      return;
+    }
+
+    const {
+      elements: { name, email, cpf, entry },
+    } = e.currentTarget;
+
+    const currentErrors = {};
+
+    if (!isValidCPF(cpf)) currentErrors.cpf = true;
+
+    if (Object.keys(currentErrors).length) {
+      setErros((state) => ({ ...state, ...currentErrors }));
+      return;
+    }
+
+    // TODO: fazer post
+  }
+
   return (
     <Layout title={title} subtitle={subtitle}>
       <div className="mx-auto overflow-hidden rounded-lg shadow-lg lg:flex lg:max-w-fit">
-        <div className="space-y-4 bg-white px-6 py-8 lg:space-y-6 lg:p-12">
+        <form
+          className="space-y-4 bg-white px-6 py-8 lg:space-y-6 lg:p-12"
+          onSubmit={handleSubmit}
+        >
           <h3 className="text-2xl font-extrabold text-gray-900 sm:text-3xl">
             Informações básicas
           </h3>
 
           <label className="flex flex-col">
-            <span className="mb-1 text-gray-500">Nome completo</span>
-            <input className="input input-primary" type="text" name="name" />
+            <span className="mb-1 text-gray-500">
+              Nome completo
+              {errors.name && (
+                <em className="text-error ml-1 text-xs font-bold not-italic">
+                  (inválido)
+                </em>
+              )}
+            </span>
+            <input
+              required
+              className="input input-primary"
+              type="text"
+              name="name"
+              placeholder="Qual o seu nome?"
+              autoFocus
+            />
           </label>
 
           <label className="flex flex-col">
-            <span className="mb-1 text-gray-500">Email</span>
-            <input className="input input-primary" type="email" name="email" />
+            <span className="mb-1 text-gray-500">
+              Email
+              {errors.email && (
+                <em className="text-error ml-1 text-xs font-bold not-italic">
+                  (inválido)
+                </em>
+              )}
+            </span>
+            <input
+              required
+              className="input input-primary"
+              type="email"
+              name="email"
+              placeholder="Digite seu email"
+            />
           </label>
 
           <label className="flex flex-col">
-            <span className="mb-1 text-gray-500">CPF</span>
-            <input className="input input-primary" type="cpf" name="cpf" />
+            <span className="mb-1 text-gray-500">
+              CPF
+              {errors.cpf && (
+                <em className="text-error ml-1 text-xs font-bold not-italic">
+                  (inválido)
+                </em>
+              )}
+            </span>
+            <IMaskInput
+              mask={'000.000.000-00'}
+              required
+              className="input input-primary"
+              type="cpf"
+              name="cpf"
+              placeholder="Digite seu CPF"
+            />
           </label>
 
           <label className="flex flex-col">
-            <span className="mb-1 text-gray-500">Valor de entrada</span>
-            <input className="input input-primary" type="number" name="entry" />
+            <span className="mb-1 text-gray-500">
+              Valor de entrada
+              {errors.entry && (
+                <em className="text-error ml-1 text-xs font-bold not-italic">
+                  (inválido)
+                </em>
+              )}
+            </span>
+            <IMaskInput
+              required
+              name="entry"
+              className="input input-primary"
+              mask={Number}
+              scale={2}
+              signed={false}
+              thousandsSeparator={''}
+              padFractionalZeros={true}
+              normalizeZeros={true}
+              radix={','}
+              mapToRadix={['.']}
+              min={0}
+              max={100000}
+              placeholder="Qual o valor de entrada?"
+            />
           </label>
 
           <div className="flex justify-between pt-1 lg:pt-2">
@@ -45,7 +146,7 @@ export function Information() {
               Salvar e continuar
             </button>
           </div>
-        </div>
+        </form>
       </div>
     </Layout>
   );
