9e4c0977a5cf81245a8e2ef3faaa8c31b7f1bd1f
Jon Loureiro <me@jonloureiro.dev>
2022-03-22 21:48:49 -0400

    chore: add text (label) to radio inputs

--- front/src/pages/Information.jsx
+++ front/src/pages/Information.jsx
@@ -122,63 +122,67 @@ export function Information() {
             />
           </label>
 
-          <div className="flex items-center space-x-4">
-            <label className="flex grow cursor-pointer">
-              <input
-                type="radio"
-                name="entry"
-                className="peer hidden"
-                value="0.1"
-              />
-              <span className="peer-checked:border-neutral w-full rounded-md border-2 py-2 text-center text-base font-bold text-gray-600 peer-checked:text-gray-800">
-                <span className="mr-0.5 text-xs not-italic text-gray-500">
-                  R$
+          <div>
+            <p className="mb-1 text-gray-500">Valor de entrada</p>
+            <div className="flex items-center space-x-4">
+              <label className="flex grow cursor-pointer">
+                <input
+                  type="radio"
+                  name="entry"
+                  className="peer hidden"
+                  value="0.1"
+                  defaultChecked
+                />
+                <span className="peer-checked:border-neutral w-full rounded-md border-2 py-2 text-center text-base font-bold text-gray-600 peer-checked:text-gray-800">
+                  <span className="mr-0.5 text-xs not-italic text-gray-500">
+                    R$
+                  </span>
+                  {course &&
+                    ((course.price / 100) * 0.1).toLocaleString(undefined, {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}
                 </span>
-                {course &&
-                  ((course.price / 100) * 0.1).toLocaleString(undefined, {
-                    minimumFractionDigits: 2,
-                    maximumFractionDigits: 2,
-                  })}
-              </span>
-            </label>
+              </label>
 
-            <label className="flex grow cursor-pointer">
-              <input
-                type="radio"
-                name="entry"
-                className="peer hidden"
-                value="0.2"
-              />
-              <span className="peer-checked:border-neutral w-full rounded-md border-2 py-2 text-center text-base font-bold text-gray-600 peer-checked:text-gray-800">
-                <span className="mr-0.5 text-xs not-italic text-gray-500">
-                  R$
+              <label className="flex grow cursor-pointer">
+                <input
+                  type="radio"
+                  name="entry"
+                  className="peer hidden"
+                  value="0.2"
+                />
+                <span className="peer-checked:border-neutral w-full rounded-md border-2 py-2 text-center text-base font-bold text-gray-600 peer-checked:text-gray-800">
+                  <span className="mr-0.5 text-xs not-italic text-gray-500">
+                    R$
+                  </span>
+                  {course &&
+                    ((course.price / 100) * 0.2).toLocaleString(undefined, {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}
                 </span>
-                {course &&
-                  ((course.price / 100) * 0.2).toLocaleString(undefined, {
-                    minimumFractionDigits: 2,
-                    maximumFractionDigits: 2,
-                  })}
-              </span>
-            </label>
+              </label>
 
-            <label className="flex grow cursor-pointer">
-              <input
-                type="radio"
-                name="entry"
-                className="peer hidden"
-                value="0.3"
-              />
-              <span className="peer-checked:border-neutral w-full rounded-md border-2 py-2 text-center text-base font-bold text-gray-600 peer-checked:text-gray-800">
-                <span className="mr-0.5 text-xs not-italic text-gray-500">
-                  R$
+              <label className="flex grow cursor-pointer">
+                <input
+                  type="radio"
+                  name="entry"
+                  className="peer hidden"
+                  value="0.3"
+                />
+                <span className="peer-checked:border-neutral w-full rounded-md border-2 py-2 text-center text-base font-bold text-gray-600 peer-checked:text-gray-800">
+                  <span className="mr-0.5 text-xs not-italic text-gray-500">
+                    R$
+                  </span>
+                  {course &&
+                    ((course.price / 100) * 0.3).toLocaleString(undefined, {
+                      minimumFractionDigits: 2,
+                      maximumFractionDigits: 2,
+                    })}
                 </span>
-                {course &&
-                  ((course.price / 100) * 0.3).toLocaleString(undefined, {
-                    minimumFractionDigits: 2,
-                    maximumFractionDigits: 2,
-                  })}
-              </span>
-            </label>
+              </label>
+            </div>
           </div>
 
           <div className="flex justify-between">
