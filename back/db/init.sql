7ec82447781a325371057429cb14801bbec73095
Jon Loureiro <me@jonloureiro.dev>
2022-03-22 23:47:48 -0400

    fix: set email to nullable

--- back/db/init.sql
+++ back/db/init.sql
@@ -9,7 +9,7 @@ CREATE TABLE courses (
 CREATE TABLE customers (
     id SERIAL PRIMARY KEY,
     name character varying(255) NOT NULL,
-    email character varying(255) NOT NULL UNIQUE,
+    email character varying(255) NOT NULL,
     cpf character varying(11) NOT NULL UNIQUE,
     created_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL,
     updated_at timestamp with time zone DEFAULT CURRENT_TIMESTAMP NOT NULL
